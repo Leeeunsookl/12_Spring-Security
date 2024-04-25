948a21ec414018f213707f916390119da9ac8ab8
pyunghun <raccoon.ohgiraffers@gmail.com>
2024-04-25 13:07:19 +0900

    [commit]

---
+++ 01_lecture-source/chap01-session-security-lecture-source/src/main/java/com/ohgiraffers/sessionsecurity/user/model/dto/LoginUserDTO.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.sessionsecurity.user.model.dto;
+
+import com.ohgiraffers.sessionsecurity.common.UserRole;
+
+public class LoginUserDTO {
+
+    private int userCode;
+    private String userId;
+    private String userName;
+    private String password;
+    private UserRole userRole;
+
+}
