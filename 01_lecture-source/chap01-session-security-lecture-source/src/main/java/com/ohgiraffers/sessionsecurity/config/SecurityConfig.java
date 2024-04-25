768799af899ffe37ebaf86701efc9733895d49de
pyunghun <raccoon.ohgiraffers@gmail.com>
2024-04-25 16:00:38 +0900

    commit

--- 01_lecture-source/chap01-session-security-lecture-source/src/main/java/com/ohgiraffers/sessionsecurity/config/SecurityConfig.java
+++ 01_lecture-source/chap01-session-security-lecture-source/src/main/java/com/ohgiraffers/sessionsecurity/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.ohgiraffers.sessionsecurity.config;
 
 import com.ohgiraffers.sessionsecurity.common.UserRole;
+import com.ohgiraffers.sessionsecurity.config.handler.AuthFailHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,11 +12,15 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 /* spring security 의 기능을 활성화 시키는 어노테이션 */
 public class SecurityConfig {
+
+    @Autowired
+    private AuthFailHandler authFailHandler;
 
     /* 필기.
     *   비밀번호를 인코딩 하기 위한 Bean
@@ -42,11 +48,38 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests( auth -> {
+            /* permitAll -> 내부에 전달한 http 요청에 대해 모두에게 허용한다. */
             auth.requestMatchers("/auth/login", "/user/signup", "/auth/fail", "/", "/main").permitAll();
+            /* hasAnyAuthority -> 전달 http 요청에 대해 권한이 있는 사람만 허용한다. */
             auth.requestMatchers("/admin/*").hasAnyAuthority(UserRole.ADMIN.getRole());
             auth.requestMatchers("/user/*").hasAnyAuthority(UserRole.USER.getRole());
+            /* 인증이 된 사람들에게 요청을 허락한다. */
             auth.anyRequest().authenticated();
-        })
+
+        }).formLogin(login -> {
+            /* 로그인 페이지에 해당되는 서블릿이 존재해야 한다. */
+            login.loginPage("/auth/login");
+            login.usernameParameter("user");    // 사용자 id 입력 필드(input 태그의 name 속성과 일치)
+            login.passwordParameter("pass");    // 사용자 pass 입력 필드(input 태그의 name 속성과 일치)
+            login.defaultSuccessUrl("/", true); // 로그인 성공 시 이동할 페이지(서블릿이 존재해야 한다.)
+            login.failureHandler(authFailHandler);     // 로그인에 실패 했을 시 처리할 커스텀 핸들러
+
+        }).logout( logout -> {
+            logout.logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout"));
+            logout.deleteCookies("JSESSIONID"); // 로그아웃 시 사용자의 JSESSIONID 삭제
+            logout.invalidateHttpSession(true); // 세션 소멸 허용
+            logout.logoutSuccessUrl("/");       // 로그아웃 시 이동할 페이지 설정
+
+        }).sessionManagement( session -> {
+            session.maximumSessions(1); // session 의 허용 갯수 제한, -> 한 사용자가 여러 창을 띄워 동시에 여러 개 세션 활성화 방지.
+            session.invalidSessionUrl("/"); // 세션이 만료 되었을 때 이동할 페이지
+
+        }).rememberMe(remember -> {
+            remember.tokenValiditySeconds(604800); // 토큰의 유효 기간 설정(1주일)
+            remember.key("mykey");  // remember-me 토큰을 생성하는데 사용되는 키
+        }).csrf( csrf -> csrf.disable()); // 추가적인 구현이 필요하기 때문에 비활성화
+
+        return http.build();
     }
 
 }
